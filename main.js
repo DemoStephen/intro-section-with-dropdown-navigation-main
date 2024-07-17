21957df3771d4976ed20c795091f04d2f6630205
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-07-17 23:54:23 +0100

    Added an event listener to the menu icon and the cancle icon n the mobile view of the website

---
+++ main.js
@@ -0,0 +1,12 @@
+const showSideBar = document.querySelector('[data-showSideBar]')
+const hideSideBar = document.querySelector('[data-hideSideBar]')
+const sideBar = document.querySelector('[data-sideBar]')
+
+showSideBar.addEventListener('click', (event)=>{
+    event.preventDefault();
+    sideBar.classList.remove('d-none');
+});
+hideSideBar.addEventListener('click', (event)=>{
+    event.preventDefault();
+    sideBar.classList.add('d-none');
+});
