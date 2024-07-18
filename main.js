515b1e9effcd6bf21b9732b2878dcac445602568
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-07-18 13:31:01 +0100

    Added new update to my html and css file

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const showSideBar = document.querySelector('[data-showSideBar]')
 const hideSideBar = document.querySelector('[data-hideSideBar]')
 const sideBar = document.querySelector('[data-sideBar]')
-
 showSideBar.addEventListener('click', (event)=>{
     event.preventDefault();
     sideBar.classList.remove('d-none');
